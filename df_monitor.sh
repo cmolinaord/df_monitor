e4b4befb5fb26b056bee5cb0cad663e5f0208fbe
cmolinaord <carlosmolina.ord@gmail.com>
2017-04-18 23:54:47 +0200

    Added functionallity for specify path root or home

--- df_monitor.sh
+++ df_monitor.sh
@@ -4,6 +4,8 @@
 
 RUNDIR="$HOME/.config/df_monitor"
 FILE_ROOT="$RUNDIR/root.dat"
+FILE_HOME="$RUNDIR/home.dat"
+RUTA=$1
 
 function create_fs_file {
 	mkdir -p $RUNDIR
@@ -11,14 +13,19 @@ function create_fs_file {
 		echo -e "year\tmonth\tday\thour\tmin\tdateNum\tUsage(%)" > $FILE_ROOT
 		echo    "*********************" >> $FILE_ROOT
 	fi
+	if [[ ! -f $FILE_HOME ]]; then
+		echo -e "year\tmonth\tday\thour\tmin\tdateNum\tUsage(%)" > $FILE_HOME
+		echo    "*********************" >> $FILE_HOME
+	fi
+
 }
 
-data_root=$(df /|grep "/dev")
+data_ruta=$(df $RUTA | grep "/dev")
 
-total=$(echo $data_root|cut -d" " -f2)
-used=$(echo $data_root|cut -d" " -f3)
+total=$(echo $data_ruta | cut -d" " -f2)
+used=$(echo $data_ruta | cut -d" " -f3)
 
-percent_used=$(expr "$used/$total*100" | bc -l)
+percent_used=$(expr "$used / $total*100" | bc -l)
 
 year=$(date +%Y)
 month=$(date +%m)
@@ -29,4 +36,12 @@ date_num=$(expr "$year + $month/12 + $day/12/30 + $hour/12/30/24" | bc -l)
 
 create_fs_file
 
-printf "%i\t%i\t%i\t%i\t%i\t%4.4f\t%2.4f\n" $year $month $day $hour $minute $date_num $percent_used >> $FILE_ROOT
+if [ $RUTA == '/' ]
+then
+	printf "%i\t%i\t%i\t%i\t%i\t%4.4f\t%2.4f\n" $year $month $day $hour $minute $date_num $percent_used >> $FILE_ROOT
+elif [[ $RUTA == '/home' || $RUTA == '/home/' ]]
+then
+		printf "%i\t%i\t%i\t%i\t%i\t%4.4f\t%2.4f\n" $year $month $day $hour $minute $date_num $percent_used >> $FILE_HOME
+else
+	echo "ERROR: Please specify a path"
+fi
